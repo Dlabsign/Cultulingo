7065f265c4e7c9cd3501a59ec1bc973752752456
Dlabsign <danielkurnip02@gmail.com>
2024-09-14 02:10:40 +0700

    update_splashScreen1

--- lib/splash/splashscreen.dart
+++ lib/splash/splashscreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class SplashScreen1 extends StatelessWidget {
@@ -7,16 +8,29 @@ class SplashScreen1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final screenSize = MediaQuery.of(context).size;
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.width - 120;
+
     final double paddingHorizontal = screenSize.width * 0.08;
 
     return Scaffold(
       body: Column(
         children: [
+          Container(
+            width: screenWidth,
+            height: screenHeight,
+            child: AspectRatio(
+                aspectRatio: 1,
+                child: SvgPicture.asset(
+                  "assets/splash/splash1.svg",
+                  fit: BoxFit.contain,
+                )),
+          ),
           Expanded(
             child: Container(
               padding: EdgeInsets.symmetric(
                 horizontal: paddingHorizontal,
-                vertical: screenSize.height * 0.05,
+                vertical: screenSize.height * 0.03,
               ),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -26,21 +40,20 @@ class SplashScreen1 extends StatelessWidget {
                       Text(
                         'Welcome to Cultulingo',
                         textAlign: TextAlign.center,
-                        style: TextStyle(
+                        style: GoogleFonts.jost(
                           color: Colors.black,
                           fontSize: screenSize.width * 0.09,
-                          fontFamily: 'Inter',
                           fontWeight: FontWeight.w500,
+                          height: 1.2,
                         ),
                       ),
-                      const SizedBox(height: 10),
+                      const SizedBox(height: 18),
                       Text(
                         'Dengan Cultulingo, Anda dapat belajar bahasa baru dengan cara yang menyenangkan dan interaktif, langsung dari ponsel Anda.',
                         textAlign: TextAlign.center,
-                        style: TextStyle(
+                        style: GoogleFonts.jost(
                           color: Colors.black,
                           fontSize: screenSize.width * 0.030, // Font dinamis
-                          fontFamily: 'Jost',
                           fontWeight: FontWeight.w400,
                           letterSpacing: 0.28,
                         ),
@@ -50,54 +63,28 @@ class SplashScreen1 extends StatelessWidget {
                   Container(
                     child: Column(
                       children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Container(
-                              width: 10,
-                              height: 10,
-                              decoration: const ShapeDecoration(
-                                color: Color(0xFF134CD2),
-                                shape: OvalBorder(),
+                        ElevatedButton(
+                          style: TextButton.styleFrom(
+                              backgroundColor: Color(
+                                0xFF134CD2,
                               ),
-                            ),
-                            const SizedBox(width: 10),
-                            Opacity(
-                              opacity: 0.50,
-                              child: Container(
-                                width: 10,
-                                height: 10,
-                                decoration: const ShapeDecoration(
-                                  color: Color(0xFF939393),
-                                  shape: OvalBorder(),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        Container(
-                          width: double.infinity,
-                          padding: EdgeInsets.all(20),
-                          decoration: BoxDecoration(
-                            color: Color(0xFF1C92FF),
-                          ),
+                              padding: EdgeInsets.all(20)),
+                          onPressed: () {},
                           child: Row(
-                            
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
                             children: [
                               Text(
                                 "Selanjutnya",
                                 style: GoogleFonts.jost(
-                                  fontSize: 12,
+                                  fontSize: 14,
                                   color: Colors.white,
                                   fontWeight: FontWeight.w600,
                                 ),
-                              )
+                              ),
                             ],
                           ),
-                        )
+                        ),
                       ],
                     ),
                   )
@@ -105,6 +92,16 @@ class SplashScreen1 extends StatelessWidget {
               ),
             ),
           ),
+          Container(
+            width: double.infinity,
+            height: 55,
+            decoration:
+                BoxDecoration(color: const Color.fromARGB(248, 255, 255, 255)),
+            child: Image.asset(
+              "assets/bg-batik.png",
+              fit: BoxFit.cover,
+            ),
+          )
         ],
       ),
     );
